a1465ee2e84445df24661e420caae1f1452df631
ishidah4954 <ttt4954@gmail.com>
2022-03-03 00:53:12 +0000

    Initial Commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9ef1a59c1c9eee64810cf7eaf80f16cce0c0272885eaea66fa8d387a85958260dd639241b0b1e0ab6ab69ea3fe76d0a7310b5a46b54a65a833cac5d9d44408d6'
